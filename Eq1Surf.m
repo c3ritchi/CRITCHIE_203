208610c653a65895c477aa67e738ab7273f1d464
Cameron Ritchie <c3ritchi@uwaterloo.ca>
2022-10-03 18:34:19 -0400

    up to q3

---
+++ Eq1Surf.m
@@ -0,0 +1,9 @@
+
+clc
+clear
+
+[x, y] = meshgrid(-2:0.01:2);
+
+z = ((-cos(x.^3.*y-8.*x)+sqrt(abs((cos(x.^3.*y-8.*x).^2-4.*(2.*x.^4+6).*(4+x.^2+y.^4)))))./(2.*(2.*x.^4+6)));
+
+surf(x,y,z, 'EdgeColor', 'none', 'FaceAlpha', 0.7, 'FaceColor', 'r');
